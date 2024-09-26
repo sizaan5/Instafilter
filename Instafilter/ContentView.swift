1fe0af2f990285dc69e27580fe6783cdbdffbac4
Izaan Saleem <sizaan55@gmail.com>
2024-09-26 11:22:32 +0500

    Day 64/100 - Building our basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -11,53 +11,44 @@ import StoreKit
 
 struct ContentView: View {
     
-    @State private var pickerItems: [PhotosPickerItem] = []
-    @State private var selectedImages: [Image] = []
-    
-    @Environment(\.requestReview) var requestReview
+    @State private var processImage: Image?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        VStack {
+        NavigationStack {
             VStack {
-                ShareLink(item: URL(string: "https://www.instagram.com/progasm_")!, subject: Text("iOS developer"), message: Text("Check instagram account"))
-                ShareLink(item: URL(string: "https://www.medium.sizaan5.com")!) {
-                    Label("iOS tutorials", systemImage: "swift")
-                }
-                .padding()
-                let example = Image(.example1)
-                ShareLink(item: example, preview: SharePreview("I am Groot!!", image: example)) {
-                    Label("Share Groot!!", systemImage: "swift")
-                }
-                .padding()
-            }
-            .padding()
-            
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
+                Spacer()
+                
+                if let processImage {
+                    processImage
                         .resizable()
                         .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a phot"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    //share the picture
                 }
             }
-            
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .onChange(of: pickerItems) {
-            Task {
-                self.selectedImages.removeAll()
-                for item in pickerItems {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
-                    }
-                }
-            }
-        }
-        .onAppear {
-            requestReview()
-        }
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
