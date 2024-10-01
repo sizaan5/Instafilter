33c3aa068cf34bde59269437747a7ff0157f78ff
Izaan Saleem <sizaan55@gmail.com>
2024-10-01 12:14:54 +0500

    Day 67/100 - Challenge wrapup completed point 2 & 3

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -18,60 +18,83 @@ struct ContentView: View {
     
     @State private var processImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
     @State private var selectedItem: PhotosPickerItem?
     @State private var showingFilters = false
     
-    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.unsharpMask()
     let context = CIContext()
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                
-                PhotosPicker(selection: $selectedItem) {
-                    if let processImage {
-                        processImage
-                            .resizable()
-                            .scaledToFit()
-                    } else {
-                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+            ZStack {
+                LinearGradient(colors: [.white, .gray], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    
+                    PhotosPicker(selection: $selectedItem) {
+                        if let processImage {
+                            processImage
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(.rect(cornerRadius: 20))
+                                .shadow(radius: 10)
+                        } else {
+                            ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                        }
                     }
-                }
-                .buttonStyle(.plain)
-                .onChange(of: selectedItem, loadImage)
-                
-                Spacer()
-                
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
-                        .disabled(processImage != nil ? false : true)
-                }
-                
-                HStack {
-                    Button("Change Filter", action: changeFilter)
-                        .disabled(processImage != nil ? false : true)
+                    .buttonStyle(.plain)
+                    .onChange(of: selectedItem, loadImage)
                     
                     Spacer()
                     
-                    if let processImage {
-                        ShareLink(item: processImage, preview: SharePreview("Instafilter image", image: processImage))
+                    HStack {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity)
+                            .onChange(of: filterIntensity, applyProcessing)
+                            .disabled(processImage != nil ? false : true)
+                            .tint(.black)
+                    }
+                    HStack {
+                        Text("Radius")
+                        Slider(value: $filterRadius)
+                            .onChange(of: filterRadius, applyProcessing)
+                            .disabled(processImage != nil ? false : true)
+                            .tint(.black)
+                    }
+                    HStack {
+                        Text("Scale")
+                        Slider(value: $filterScale)
+                            .onChange(of: filterScale, applyProcessing)
+                            .disabled(processImage != nil ? false : true)
+                            .tint(.black)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Button("Change Filter", action: changeFilter)
+                            .disabled(processImage != nil ? false : true)
+                            .tint(.black)
+                        
+                        Spacer()
+                        
+                        if let processImage {
+                            ShareLink(item: processImage, preview: SharePreview("Instafilter image", image: processImage))
+                                .tint(.black)
+                        }
                     }
                 }
-            }
-            .padding([.horizontal, .bottom])
-            .navigationTitle("Instafilter")
-            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
-                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
-                Button("Edges") { setFilter(CIFilter.edges()) }
-                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
-                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
-                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
-                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
-                Button("Vignette") { setFilter(CIFilter.vignette()) }
-                Button("Cancel", role: .cancel) { }
+                .padding([.horizontal, .bottom])
+                .navigationTitle("Instafilter")
+                .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                    Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                    Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                    Button("Vignette") { setFilter(CIFilter.vignette()) }
+                    Button("Cancel", role: .cancel) { }
+                }
             }
         }
     }
@@ -95,8 +118,8 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
@@ -111,7 +134,7 @@ struct ContentView: View {
         
         filterCount += 1
         
-        if filterCount >= 5 {
+        if filterCount >= 200 {
             requestReview()
         }
     }
